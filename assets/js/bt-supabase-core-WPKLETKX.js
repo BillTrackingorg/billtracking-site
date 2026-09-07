eeac88c996d5bfe3bbed7115c0fc304e3f3e4b58
Thomas Vanhoutte <thomas.vanhoutte@billtracking.org>
2026-09-07 17:18:19 +0300

    rebuild the web core at the app's HEAD (the protection section's app mirror)

---
+++ assets/js/bt-supabase-core-WPKLETKX.js
@@ -0,0 +1,1 @@
+import{a as b,b as c,c as d}from"./bt-chunk-7SNUWB2R.js";import{e as a}from"./bt-chunk-GI6VEB7W.js";export{a as AUTH_STORAGE_KEY,b as createSupabaseClient,c as installSupabaseClient,d as supabaseClient};
