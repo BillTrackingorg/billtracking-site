677d9e6e9ffdfd599e22cebe7e5d497baf63bede
Thomas Vanhoutte <thomas.vanhoutte@billtracking.org>
2026-09-07 18:32:47 +0300

    rebuild the web core after the comment sweep

---
+++ assets/js/bt-supabase-core-DNGCQ5BH.js
@@ -0,0 +1,1 @@
+import{a as b,b as c,c as d}from"./bt-chunk-MKENRA3O.js";import{e as a}from"./bt-chunk-2JLD2RHK.js";export{a as AUTH_STORAGE_KEY,b as createSupabaseClient,c as installSupabaseClient,d as supabaseClient};
