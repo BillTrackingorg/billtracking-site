9c7c402bcdb337876775b9a6bb070db823dea607
Thomas Vanhoutte <thomas.vanhoutte@billtracking.org>
2026-09-07 03:15:42 +0300

    rebuild the web core at the app's HEAD: the privacy date and the ruled accuracy sentence

---
+++ assets/js/bt-supabase-core-K577MUNV.js
@@ -0,0 +1,1 @@
+import{a as b,b as c,c as d}from"./bt-chunk-H6HIUHZC.js";import{e as a}from"./bt-chunk-QVSZ5A2A.js";export{a as AUTH_STORAGE_KEY,b as createSupabaseClient,c as installSupabaseClient,d as supabaseClient};
